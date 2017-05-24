ae7e5cb6033070252b0ee05d724e7be64cf9c2da
Andrey Butko <andrey.butko@gmail.com>
2017-05-25 00:13:46 +0500

    First one

---
+++ src/main/java/butnet/blvs/model/BaseEntity.java
@@ -0,0 +1,53 @@
+package butnet.blvs.model;
+
+/**
+ * Created by butkoav on 24.05.2017.
+ */
+public abstract class BaseEntity {
+
+    private Integer id;
+
+    protected BaseEntity() {
+    }
+
+    protected BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return (getId() == null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return getId() != null && getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return (getId() == null) ? 0 : getId();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Entity %s (%s)", getClass().getName(), getId());
+    }
+
+}
